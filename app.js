417ad015d441be033c9dc3a7463ce7a38223686f
ghaidan <you@example.com>
2019-04-10 10:47:22 -0700

    added more styles and pictures

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function initMap() {
     // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
+    var uluru = {lat: 37.6889088, lng: -122.469409};
     // The map, centered at Uluru
     var map = new google.maps.Map(
         document.getElementById('map'), {zoom: 4, center: uluru});
